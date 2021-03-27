2b1309871e7e8aa3b4cae5f4718f3c220c3b8e3f
Max Lareau <snowboardit@me.com>
2021-03-27 18:46:22 -0400

    Initial commit - setup collab workspace in VSCode

--- goon.sol
+++ goon.sol
@@ -1,1 +1,19 @@
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.3;
+
+contract GOONCoin {
+    
+    mapping(address => uint) public balances;
+    uint public totalSupply = 42000 * 10 ** 18;
+    string public symbol = "GOON";
+    string public name = "GOON Coin";
+    uint public decimals = 18;
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+    
+    function balanceOf(address owner) public view returns(uint) {
+        return balances[owner];
+    }
+    
+}
