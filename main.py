cafd129a369b7c60269baff4b097af243d4c2e91
Max <snowboardit@me.com>
2021-05-04 19:47:39 -0400

    introduced Python Discord bot file and a file containing the authorization token

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import "discord-config.json" as dc
+
+# Init client
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(dc.token)
