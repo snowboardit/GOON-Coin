c51c4ebfb612571bdd2956e29ed1ebc61b90c092
Max Lareau <snowboardit@me.com>
2021-05-05 00:34:48 -0400

    Added a cache system for working data. Added function/text command to get a list of users

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord
-import "discord-config.json" as dc
+import os
+import json
 
-# Init client
-client = discord.Client()
+# Init client and DB
+client = discord.Client() # init discord client
+pathToDB = "db.json" # db file
+data = {}
 
 @client.event
 async def on_ready():
+    global data
+    data = initDB()
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
@@ -13,7 +18,44 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    # Hello
+    if message.content.startswith('*hello'):
         await message.channel.send('Hello!')
 
-client.run(dc.token)
+    # Help
+    if message.content.startswith('*help'):
+        await message.channel.send('No!')
+
+    # List Users
+    if message.content.startswith('*users'):
+        _nameList = []
+        names = data.user.name
+        print(names)
+        # print each name out line by line
+        # for n in data.user.name:
+        await message.channel.send(data)
+
+def getData():
+    _data = {}
+    if os.path.exists(pathToDB):
+        with open(pathToDB) as db:
+            _data = json.load(db)
+    else:
+        _data = None
+    return _data
+
+def setData():
+    with open(pathToDB, "w") as out:
+        json.dump(data, out)
+
+def initDB():
+    _data = {}
+    if getData() is not None or getData() is not {}:
+        _data = getData()
+        print("data:\n{}".format(_data))
+    else:
+        print("no data for u")
+    return _data
+
+os.environ["token"] = "ODM4OTUwMDYxMjQ5Mzk2NzY3.YJCjIQ.kdn829zRQRpwVvjJT_nbgTWqHkI" # Set the token
+client.run(os.getenv("token"))
